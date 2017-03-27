e8d8c7928447a83848680eac49959fa5351cdf7a
phil <philip.benner@gmail.com>
2017-03-27 15:26:21 +1300

    updated dimensions and linking between classes -

--- Game.js
+++ Game.js
@@ -10,10 +10,16 @@ function Game (_g){
 		fieldWidth: 300,
 		fieldHeight : 300,
 		blockSize : 10,
+		fX : 0,
+		fY : 0,//frame X and Y 
 		maxWidth : 320,
 		maxHeight : 568 // iphone 5 stuff... 
 	};
-
+	// I really should make all these x:y: objects/arrays.
+	// I would personally look for 'vectors', or Points for this information
+	// P5 uses points as ... points. p5.vectors are vectors! but I know them as es style
+	// an assigned array. (as must languages would use and arraylist - only one type of object.)
+	// next revision.  need to learn es6 and all of p5js.
 	this.stats = {
 		score : 0,
 		highScore : 0,
@@ -21,6 +27,7 @@ function Game (_g){
 	}
 
 	this.links = {
+		main : this,
 		model : this.model,
 		game: this.game,
 		ui : this.ui
@@ -28,8 +35,13 @@ function Game (_g){
 }
 
 Game.prototype.init = function (){
+	//resolve dimensions
 	this.dimensions.wWidth = this.game.windowWidth;
 	this.dimensions.wHeight = this.game.windowHeight;
+
+	this.dimensions.fX = this.game.floor((this.dimensions.wWidth - this.dimensions.maxWidth)/2);
+	this.dimensions.fY = this.game.floor((this.dimensions.wHeight - this.dimensions.maxHeight)/2);
+
 	
 	this.game.createCanvas(this.dimensions.wWidth, this.dimensions.wHeight);
 	//console.log(this.dimensions);
@@ -38,6 +50,7 @@ Game.prototype.init = function (){
 	this.ui = new UI(this.game);
 
 	this.links = {
+		main : this,
 		game: this.game,
 		model : this.model,
 		ui : this.ui
@@ -45,8 +58,11 @@ Game.prototype.init = function (){
 	
 	this.model.init(this.links);
 	this.ui.init( this.links);
+	//console.log('home',this.dimensions)
 }
 
 Game.prototype.draw = function (){
-	this.game.background(0,0,0);	
+	console.log('game - draw')
+	this.game.background(0,0,0);
+	this.ui.show();	
 }
--- UI.js
+++ UI.js
@@ -3,7 +3,7 @@ function UI (_g){
 	this.links = {};//passed in in init
 
 	this.screenStat = {
-		modal : false,
+		gameOver : false,
 		bg : false,
 		start : false,
 		howto : false
@@ -12,15 +12,66 @@ function UI (_g){
 
 UI.prototype.init = function (_l) {
 	this.links = _l;
+	//console.log('init in ui', this.links.dimensions)
 	//console.log('test', this.links.model.sprites)
+}
+
+UI .prototype.show = function (){
+	
+	this.drawFrame();
+	this.screenStat.gameOver = true;
+	if(this.screenStat.gameOver){
+		this.drawGameOver();
+	}
+
+	
+}
+
+UI.prototype.drawFrame = function (){
+	var lp = this.links;
+	//just a frame of the dimensions of game (mobile iphone5 size)
+	// will need to make this responsive later on. lets git it fleshed out for now.
+	lp.game.push();
+	lp.game.translate(lp.main.dimensions.fX, lp.main.dimensions.fY);
+	lp.game.noFill();
+	lp.game.stroke(255);
+	lp.game.strokeWeight(1);
+	lp.game.rect(0,0,lp.main.dimensions.maxWidth,lp.main.dimensions.maxHeight);
+	lp.game.pop();
+	
 }
 
 UI.prototype.drawBG = function (){
 
 }
 
-UI.prototype.drawModal = function (){
+UI.prototype.drawGameOver = function (){
+	//console.log('draw')
+	var tempHeight = 160;
+	var lp = this.links;
+	var tempx = Math.floor((lp.main.dimensions.wWidth - lp.main.dimensions.maxWidth)/2+20);
+	var tempy = Math.floor((lp.main.dimensions.maxHeight)/2-(tempHeight/2));
 
+	lp.game.push();
+	lp.game.translate(tempx, tempy);
+	lp.game.noFill();
+	lp.game.stroke(255);
+	lp.game.strokeWeight(4);
+	lp.game.rect(0,0,lp.main.dimensions.maxWidth-40,tempHeight);
+	//
+	lp.game.noStroke();
+	lp.game.fill(255);
+	lp.game.textSize(40);
+	lp.game.translate(38, 40);
+	lp.game.text("Game Over",0,0);
+	//
+	lp.game.textSize(20);
+	lp.game.translate(38, 40);
+	lp.game.text("try again?",0,0);
+	//
+
+	//
+	lp.game.pop();
 }
 
 UI.prototype.drawStart = function (){
